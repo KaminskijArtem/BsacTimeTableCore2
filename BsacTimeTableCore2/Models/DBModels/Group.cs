8d667eb4c7b66e6927f6ebebf01dfc8e4a7141a8
Unknown <artemkaminskij2016@gmail.com>
2018-11-06 21:19:55 +0300

    Init

---
+++ BsacTimeTableCore2/Models/DBModels/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BsacTimeTableCore2.Models.DBModels
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? IdFlow { get; set; }
+        public int IdFaculty { get; set; }
+        public int EduLevel { get; set; }
+    }
+}
